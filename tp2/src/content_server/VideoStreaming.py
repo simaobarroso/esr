ddec42e4ee27f1b94be760a9cff3cb18a905882d
goncalobraga27 <goncalobraga02@gmail.com>
2023-11-07 17:07:29 +0000

    VideoStreaming

--- tp2/src/content_server/VideoStreaming.py
+++ tp2/src/content_server/VideoStreaming.py
@@ -4,4 +4,23 @@ Esta classe serve para a criação de frames para o vídeo que irá ser transmit
 class VideoStreaming:
     def __init__(self,fileVideo):
         self.fileVideo = fileVideo   # Nome do ficheiro que contêm o vídeo
-        self.numFrames = 0 # Número de frames que o vídeo possui 
+        self.numFrame = 0           # Número de frame que em que o vídeo está
+        try:
+            self.file = open(self.fileVideo, 'rb')    # Ficheiro de vídeo 
+        except:
+            print("ERRO: Erro na leitura do ficheiro de vídeo ...")
+
+    def anotherFrame(self):
+        """ Função que nos dá o próximo frame de um determinado vídeo  """
+        dados = self.file.read(5) # Obtenção do framelength dos primeiros 5 bits do vídeo 
+        if dados:
+            frameLength = int(dados)   # Irá possuir o comprimento de cada frame do vídeo
+            dados = self.file.read(frameLength) # Leitura do frame atual do vídeo que está a ser transmitido 
+            self.numFrame += 1
+        return dados
+
+    def getNumFrame(self):
+        """ Função que nos dá o frame atual do vídeo que está a ser transmitido """
+        return self.numFrame
+
+        
