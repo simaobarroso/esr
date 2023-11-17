4af72a607462f7bf1547477440b2d579028ca9bf
XubunCORE 7.5 <simao.barroso@outlook.pt>
2023-11-17 22:25:27 +0000

    Video inicio

---
+++ tp2/videocastTest/src/main_client.py
@@ -0,0 +1,32 @@
+from PyQt5.QtWidgets import QApplication
+
+from client.client_gui import ClientWindow
+
+import socket
+
+
+if __name__ == '__main__':
+    import sys
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = str(s.getsockname()[0])
+    s.close()
+
+    if len(sys.argv) < 3:
+        print(f"Usage: {sys.argv[0].split('/')[-1]} <file name> <host address> <host port> <RTP port>")
+        exit(-1)
+
+    file_name, host_address = *sys.argv[1:],
+
+    try:
+        host_port = 5540 #int(host_port)
+        rtp_port = 5541 #int(rtp_port)
+    except ValueError:
+        raise ValueError('port values should be integer')
+
+    app = QApplication(sys.argv)
+    client = ClientWindow(file_name, host_address, host_port, rtp_port,ip)
+    client.resize(400, 300)
+    client.show()
+    sys.exit(app.exec_())
---
+++ tp2/videocastTest/src/main_server.py
@@ -0,0 +1,28 @@
+from server.server import Server
+import socket
+
+
+if __name__ == '__main__':
+    import sys
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = str(s.getsockname()[0])
+    s.close()
+
+    #if len(sys.argv) != 3:
+    #    print(f"Usage: {sys.argv[0].split('/')[-1]} <port>")
+    #    exit(-1)
+    try:
+        port = 5540 #int(sys.argv[1])
+        #ip = sys.argv[2]
+    except ValueError:
+        raise ValueError('port value should be integer')
+
+    while True:
+        server = Server(port,ip)
+        try:
+            server.setup()
+            server.handle_rtsp_requests()
+        except ConnectionError as e:
+            server.server_state = server.STATE.TEARDOWN
+            print(f"Connection reset: {e}")
